2e4d6243075ee95f49db1a9ae5c465c605bb2003
Pranjal Shukla <62809277+pranjalshukla23@users.noreply.github.com>
2023-11-28 17:55:29 +0000

    google analytics integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return ( 
+  <>
+  <Script
+    src="https://www.googletagmanager.com/gtag/js?id=G-xxxxxxxxxx"
+    strategy="afterInteractive"
+  />
+  <Script id="google-analytics" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){window.dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-DCVY2WEGW9');
+    `}
+  </Script>
+
+  <Component {...pageProps} />
+</>
+)
 }
 
 export default MyApp
